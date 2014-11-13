62eacfe71e4d28edd4e3a4c59bb29e1bbcf507fc
Dave <darshit.dave@sungard.com>
2014-11-13 14:48:34 -0500

    Added Test

---
+++ FPBRTest/1_Login_feature.cs
@@ -0,0 +1,83 @@
+namespace FPBRTest
+{
+using System;
+using StepBinder;
+
+[CustomAttributes.FixtureAttr("CustomerSite")]
+[CustomAttributes.FixtureAttr("CustomerSiteLogin")]
+public class CustomerSiteLogin
+{
+	public CustomerSiteLogin()
+	{
+		FunctionBinder.CallBeforeX("CustomerSite", "", "CustomerSiteLogin", "feature");
+	}
+
+	~CustomerSiteLogin()
+	{
+		FunctionBinder.CallAfterX("CustomerSite", "", "CustomerSiteLogin", "feature");
+	}
+
+	[CustomAttributes.CaseAttr("CustomerSite")]
+	[CustomAttributes.CaseAttr("CustomerSiteLogin")]
+	public void LoginWithEmptyPassword()
+	{
+		FunctionBinder.CallBeforeX("CustomerSite", "", "CustomerSiteLogin", "4. Login with empty password", "CustomerSite.CustomerSiteLogin", "scenario");
+		FunctionBinder.Given("CustomerSite", "", "CustomerSiteLogin", "GivenIAmOnTheLoginPage");
+        //FunctionBinder.And("CustomerSite", "", "CustomerSiteLogin", "GivenIEnterInvalidUsername");
+        //FunctionBinder.And("CustomerSite", "", "CustomerSiteLogin", "GivenIEnterEmptyPassword");
+        //FunctionBinder.When("CustomerSite", "", "CustomerSiteLogin", "WhenILogin");
+        //FunctionBinder.Then("CustomerSite", "", "CustomerSiteLogin", "ThenIShouldSeeADeniedErrorMessage");
+        FunctionBinder.CallAfterX("CustomerSite", "", "CustomerSiteLogin", "scenario");
+	}
+
+	[CustomAttributes.CaseAttr("CustomerSite")]
+	[CustomAttributes.CaseAttr("CustomerSiteLogin")]
+	public void LoginWithEmptyUsername()
+	{
+		FunctionBinder.CallBeforeX("CustomerSite", "", "CustomerSiteLogin", "5. Login with empty username", "CustomerSite.CustomerSiteLogin", "scenario");
+		FunctionBinder.Given("CustomerSite", "", "CustomerSiteLogin", "GivenIAmOnTheLoginPage");
+		FunctionBinder.And("CustomerSite", "", "CustomerSiteLogin", "GivenIEnterEmptyUsername");
+		FunctionBinder.And("CustomerSite", "", "CustomerSiteLogin", "GivenIEnterInvalidPassword");
+		FunctionBinder.When("CustomerSite", "", "CustomerSiteLogin", "WhenILogin");
+		FunctionBinder.Then("CustomerSite", "", "CustomerSiteLogin", "ThenIShouldSeeADeniedErrorMessage");
+		FunctionBinder.CallAfterX("CustomerSite", "", "CustomerSiteLogin", "scenario");
+	}
+
+	[CustomAttributes.CaseAttr("CustomerSite")]
+	[CustomAttributes.CaseAttr("CustomerSiteLogin")]
+	public void LoginWithEmptyUsernameAndInvalidPassword()
+	{
+		FunctionBinder.CallBeforeX("CustomerSite", "", "CustomerSiteLogin", "6. Login with empty username and invalid password", "CustomerSite.CustomerSiteLogin", "scenario");
+		FunctionBinder.Given("CustomerSite", "", "CustomerSiteLogin", "GivenIAmOnTheLoginPage");
+		FunctionBinder.And("CustomerSite", "", "CustomerSiteLogin", "GivenIEnterEmptyUsername");
+		FunctionBinder.And("CustomerSite", "", "CustomerSiteLogin", "GivenIEnterInvalidPassword");
+		FunctionBinder.When("CustomerSite", "", "CustomerSiteLogin", "WhenILogin");
+		FunctionBinder.Then("CustomerSite", "", "CustomerSiteLogin", "ThenIShouldSeeADeniedErrorMessage");
+		FunctionBinder.CallAfterX("CustomerSite", "", "CustomerSiteLogin", "scenario");
+	}
+
+	[CustomAttributes.CaseAttr("CustomerSite")]
+	[CustomAttributes.CaseAttr("CustomerSiteLogin")]
+	public void LoginWithEmptyPasswordAndInvalidPassword()
+	{
+		FunctionBinder.CallBeforeX("CustomerSite", "", "CustomerSiteLogin", "7. Login with empty password and invalid password", "CustomerSite.CustomerSiteLogin", "scenario");
+		FunctionBinder.Given("CustomerSite", "", "CustomerSiteLogin", "GivenIAmOnTheLoginPage");
+        //FunctionBinder.And("CustomerSite", "", "CustomerSiteLogin", "GivenIEnterEmptyUsername");
+        //FunctionBinder.And("CustomerSite", "", "CustomerSiteLogin", "GivenIEnterInvalidPassword");
+        //FunctionBinder.When("CustomerSite", "", "CustomerSiteLogin", "WhenILogin");
+        //FunctionBinder.Then("CustomerSite", "", "CustomerSiteLogin", "ThenIShouldSeeADeniedErrorMessage");
+        FunctionBinder.CallAfterX("CustomerSite", "", "CustomerSiteLogin", "scenario");
+	}
+
+	[CustomAttributes.CaseAttr("CustomerSite")]
+	[CustomAttributes.CaseAttr("CustomerSiteLogin")]
+	public void LoginWithTooManyInvalidAttempts()
+	{
+		FunctionBinder.CallBeforeX("CustomerSite", "", "CustomerSiteLogin", "8. Login with too many invalid attempts", "CustomerSite.CustomerSiteLogin", "scenario");
+		FunctionBinder.Given("CustomerSite", "", "CustomerSiteLogin", "GivenIAmOnTheLoginPage");
+		FunctionBinder.And("CustomerSite", "", "CustomerSiteLogin", "GivenIAttemptToLoginWithTooManyInvalidAttempts");
+		FunctionBinder.Then("CustomerSite", "", "CustomerSiteLogin", "ThenIShouldBeLockedOut");
+		FunctionBinder.CallAfterX("CustomerSite", "", "CustomerSiteLogin", "scenario");
+	}
+}
+}
---
+++ FPBRTest/FPBR.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StepBinder;
+using YetAnotherRunner;
+
+
+namespace FPBRTest
+{
+    [TestClass]
+    public class FPBR
+    {
+        public FPBR()
+        {
+            string actualPath = @"C:\_Automation\test_nunit_test\SunGard.PNE.Test.sln";
+            string slnDirPath = Path.GetDirectoryName(actualPath) + @"\";
+            Executor exe = new Executor();
+            Solution sln = new Solution(actualPath);
+            string findTestProj = "specs";
+            List<DllInfo> addRefList = new List<DllInfo>();
+            foreach (SolutionProject sp in sln.Projects)
+            {
+                string[] pName = sp.ProjectName.Split('.');
+                if (StringComparer.OrdinalIgnoreCase.Equals(pName.Last<string>(), findTestProj))
+                {
+                    DllInfo info = new DllInfo();
+                    info.name = sp.ProjectName;
+                    info.path = slnDirPath + Path.GetDirectoryName(sp.RelativePath) + @"\bin\debug\" + sp.RelativePath.Split('\\').Last<string>().Replace(".csproj", ".dll");
+                    addRefList.Add(info);
+                }
+            }
+            new StepLoader(addRefList);
+        }
+
+        [TestMethod]
+        public void StepLoaderTest()
+        {
+            CustomerSiteLogin csl = new CustomerSiteLogin();
+            CustomerSiteLogin csl1 = new CustomerSiteLogin();
+            //csl.LoginWithEmptyPassword();
+            //csl1.LoginWithEmptyPasswordAndInvalidPassword();
+            Task t1 = Task.Factory.StartNew(() => csl.LoginWithEmptyPassword());
+            Task t2 = Task.Factory.StartNew(() => csl.LoginWithEmptyPasswordAndInvalidPassword());
+            Task.WaitAll(t1, t2);
+        }
+    }
+}
