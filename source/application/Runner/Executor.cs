0ff1e6e07d95377717469a8cfc58a5f13d16037f
ddave09 <dd1691@nyu.edu>
2015-02-03 17:56:28 -0500

    Removed loops from Executor class

--- source/application/Runner/Executor.cs
+++ source/application/Runner/Executor.cs
@@ -131,50 +131,6 @@
             Console.WriteLine("\nMethod Handler Completed\n");
         }
         
-        internal void ExecuteTest(List<DllInfo> dlls)
-        {
-            ILogManager logActivatorException = new Logger();
-            Assembly runtimeAssembly = null;
-            GlobalTestStates.GrandStartTime = DateTime.Now.Ticks;
-            foreach (DllInfo dllinfo in dlls)
-            {
-                runtimeAssembly = Assembly.Load(AssemblyName.GetAssemblyName(dllinfo.path));
-                List<Type> types = TestTypes(runtimeAssembly.GetTypes().ToList());
-                
-                foreach (Type type in types)
-                {
-                    IndividualFeatureTestState itfs = new IndividualFeatureTestState();
-                    Object obj = null;
-                    try
-                    {
-                        obj = Activator.CreateInstance(type);
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine("Could not find Step class, Most Probable: Check feature file namespace or step class name");
-                        logActivatorException.Fatal("Could not find Step class, Most Probable: Check feature file namespace or step class name", e);
-                        System.Environment.Exit(-1);
-                    }
-                    
-                    MethodInfo TearDownFeature = null;
-                    List<MethodInfo> methods = TestMethods(type.GetMethods().ToList(), ref TearDownFeature);
-                    itfs.FeatureName = type.FullName;
-                    itfs.StartTick = DateTime.Now.Ticks;
-                    foreach (MethodInfo method in methods)
-                    {
-                        List<string> attrsValues = this.GetAttributesConstructorValues(method);
-                        GlobalTestStates.IncrementScenarioCount();
-                        run.Run(obj, method, type, attrsValues, bw);
-                    }
-                    while (Convert.ToBoolean(GlobalTestStates.GetScenarioCount)) ;
-                    TearDownFeature.Invoke(obj, null);
-                    itfs.EndTick = DateTime.Now.Ticks;
-                    GlobalTestStates.AddFeature(itfs);
-                }
-            }
-            GlobalTestStates.GrandEndTime = DateTime.Now.Ticks;
-        }
-
         private List<string> GetAttributesConstructorValues(MethodInfo method)
         {
             List<string> attrValues = new List<string>();
