febf685de6f822d75271076e177517b8773428b5
Bryant, Charles <newjaxcom@gmail.com>
2015-10-07 12:37:33 -0400

    Adding build project. Adding namespace grouping unit test.

---
+++ source/application/Tarsvin.Build/Nake.bat
@@ -0,0 +1,2 @@
+@ECHO OFF 
+tools\Nake.exe %*
---
+++ source/application/Tarsvin.Build/tools/Tarsvin/tarsvin.psm1
@@ -0,0 +1,13 @@
+﻿#PROPERTIES
+
+# INSTALL
+function Install-Tarsvin($package) {
+  $id = $package.Id;
+  $version = $package.Version;
+}
+
+# UNINSTALL
+function Uninstall-Tarsvin($package) {
+  $id = $package.Id;
+  $version = $package.Version;
+}
