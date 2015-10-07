3638186c6c214c2d48f24701fd92ba2ea442bc4d
Bryant, Charles <newjaxcom@gmail.com>
2015-10-07 12:37:33 -0400

    Adding build project. Adding namespace grouping unit test.

---
+++ source/application/Tarsvin.Build/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Reflection;
+
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyTitle("Tarsvin")]
+[assembly: AssemblyCopyright("Copyright (c) 2014, Darshit Dave")]
+[assembly: AssemblyTrademark("Trademark by Darshit Dave")]
+[assembly: AssemblyCompany("Darshit Dave")]
+[assembly: AssemblyProductAttribute("Tarsvin")]
---
+++ source/test/RunnerHelper.Test/NamespaceGroupingTest.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tarsvin.Runner;
+
+namespace RunnerHelper.Test
+{
+    [TestClass]
+    public class NamespaceGroupingTest
+    {
+        ICollection<IndividualTestState> tests;
+
+        [TestMethod]
+        public void Can_Count_Unique_Namespaces()
+        {
+            this.tests = new List<IndividualTestState>();
+
+            IndividualTestState test = new IndividualTestState();
+            test.NameSpace = "A.B.C.a";
+            test.TestName = "a";
+            tests.Add(test);
+
+            test = new IndividualTestState();
+            test.NameSpace = "A.B.C.b";
+            test.TestName = "b";
+            tests.Add(test);
+
+            test = new IndividualTestState();
+            test.NameSpace = "A.B.c";
+            test.TestName = "c";
+            tests.Add(test);
+
+            test = new IndividualTestState();
+            test.NameSpace = "A.B.a";
+            test.TestName = "a";
+            tests.Add(test);
+
+            test = new IndividualTestState();
+            test.NameSpace = "A.B.C.D.e";
+            test.TestName = "e";
+            tests.Add(test);
+
+            var grouped = tests.GroupBy(x => new { x.NameSpace });
+
+            Assert.AreEqual(5, grouped.Count());
+        }
+
+        [TestMethod]
+        public void Can_Group_Duplicate_Namespaces()
+        {
+            this.tests = new List<IndividualTestState>();
+
+            IndividualTestState test = new IndividualTestState();
+            test.NameSpace = "A.B.C";
+            test.TestName = "a";
+            tests.Add(test);
+
+            test = new IndividualTestState();
+            test.NameSpace = "A.B.C";
+            test.TestName = "b";
+            tests.Add(test);
+
+            test = new IndividualTestState();
+            test.NameSpace = "A.B";
+            test.TestName = "c";
+            tests.Add(test);
+
+            test = new IndividualTestState();
+            test.NameSpace = "A.B";
+            test.TestName = "a";
+            tests.Add(test);
+
+            test = new IndividualTestState();
+            test.NameSpace = "A.B.C.D";
+            test.TestName = "e";
+            tests.Add(test);
+
+            var grouped = tests.GroupBy(x => new { x.NameSpace });
+
+            Assert.AreEqual(3, grouped.Count());
+        }
+
+
+    }
+}
